d7143ee38bf7ca00e2684bbfd87ec1a65fa6aca8
Pedro Siccha <pedro.d.siccha@gmail.com>
2024-10-10 19:09:30 -0500

    Corrige Hisoty

--- src/app/features/consultar-acto-inseguro/consultar-acto-inseguro.component.ts
+++ src/app/features/consultar-acto-inseguro/consultar-acto-inseguro.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { Router } from '@angular/router';
+import { PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-consultar-acto-inseguro',
@@ -13,11 +15,22 @@ import { Router } from '@angular/router';
 })
 export class ConsultarActoInseguroComponent implements OnInit {
   detalle: any;
-  constructor(private router: Router) {}
+
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object // Inyección de platformId para verificar si estamos en el navegador
+  ) {}
+
   ngOnInit(): void {
-    this.detalle = history.state.data;
-    console.log("Detalle recibido:", this.detalle);
+    if (isPlatformBrowser(this.platformId)) {
+      // Este bloque solo se ejecuta en el navegador
+      this.detalle = history.state.data;  // Acceso a history.state de forma segura
+      console.log("Detalle recibido:", this.detalle);
+    } else {
+      console.log("SSR: No hay acceso a 'history' en el servidor");
+    }
   }
+
   navigateToConsultarActoInseguro() {
     this.router.navigate(['/consultar-acto-inseguro']);
   }
