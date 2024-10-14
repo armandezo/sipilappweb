62bc097b849a6159b9f84e2ba5bf1e7852f4359c
Pedro Siccha <pedro.d.siccha@gmail.com>
2024-10-14 10:14:07 -0500

    fix buttom

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZBBBBFSN.mjs";import"./chunk-4OPAFGY2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-QZW72OR4.mjs";import"./chunk-4OPAFGY2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
