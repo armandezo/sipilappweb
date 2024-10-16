76f2702a6608ec586a59b6e01f55c02ba3f06db8
Pedro Siccha <pedro.d.siccha@gmail.com>
2024-10-16 00:21:08 -0500

    fix list

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-7ATEIIK2.mjs";import"./chunk-4OPAFGY2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-TNYG6W3V.mjs";import"./chunk-4OPAFGY2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
