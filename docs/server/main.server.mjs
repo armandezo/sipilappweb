079a26fbdfe6de03a4fff97e69743f6fe0bdd0f5
ricardoalonne <ricardo.alonne@gmail.com>
2024-11-18 18:21:02 -0500

    test: deploy test.

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TNYG6W3V.mjs";import"./chunk-4OPAFGY2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-7OLPTI3O.mjs";import"./chunk-4OPAFGY2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
